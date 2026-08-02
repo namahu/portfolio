e4bfdb236a07972f17d790564c3c624324467768
namahu <neti0326@gmail.com>
2026-08-02 15:12:10 +0900

    feat: implement ProjectList component

--- packages/frontend/src/features/projects/components/project-list.tsx
+++ packages/frontend/src/features/projects/components/project-list.tsx
@@ -9,13 +9,23 @@ export function ProjectList({ projectPromise }: ProjectListProps) {
   const projects = use(projectPromise);
 
   return (
-    <div>
+    <div className="m-4 flex gap-4">
       {projects.map(project => (
-        <div key={project.id}>
+        <div key={project.id}
+          className="w-lg bg-zinc-100 p-2 border border-zinc-300 rounded-xl flex flex-col gap-2"
+        >
           <h2>{project.title}</h2>
-          <p>{project.description}</p>
-          <span>{project.techStack.join(", ")}</span>
-          <span>{project.link}</span>
+          <div className="mx-4">
+            <p>{project.description}</p>
+          </div>
+          <div className="mx-4 flex flex-col items-start">
+            <span className="font-semibold">技術スタック</span>
+            <span className="mx-4">{project.techStack.join(", ")}</span>
+          </div>
+          <div className="mx-4 flex flex-col items-start">
+            <span className="font-semibold">Link</span>
+            <span className="mx-4">{project.link}</span>
+          </div>
         </div>
       ))}
     </div>
