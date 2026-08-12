10447a03fe7f14ef7dfa9fc568e3429b26eb11a6
namahu <neti0326@gmail.com>
2026-08-12 18:42:33 +0900

    feat: implement responsive design

--- packages/frontend/src/components/project-card.tsx
+++ packages/frontend/src/components/project-card.tsx
@@ -13,69 +13,65 @@ export function ProjectCard({ type = "personal", project }: ProjectCardProps) {
 
   return (
     <div
-      className="w-116 h-max bg-zinc-200 px-2 py-4 border border-zinc-300 rounded-xl flex flex-col gap-2"
+      className="w-full h-full bg-zinc-200 px-2 py-4 border border-zinc-300 rounded-xl flex flex-col gap-2"
     >
       {/* Card Title */}
-      <h2>{type === "oss" ? project.repository?.name : project.title}</h2>
+      <h3>{type === "oss" ? project.repository?.name : project.title}</h3>
 
       {/* Card Body */}
-      <div className="mx-4 flex flex-col gap-2">
-        <div>
-          <p>{project.description}</p>
-        </div>
-        <div className="flex flex-col items-start gap-2">
-          <span className="font-semibold">Tech Stack</span>
-          <div className="mx-2 flex flex-row flex-wrap gap-1">
-            {project.techStack.map((tech) => (
-              <span key={tech}
-                className="bg-zinc-50 border border-zinc-300 rounded-xl text-xs mx-1 px-4 py-1"
-              >
-                {tech}
-              </span>
-            ))}
+      <div className="mx-4 flex-1 flex flex-col justify-between gap-2">
+        <div className="flex flex-col gap-2">
+          <div>
+            <p>{project.description}</p>
           </div>
-        </div>
-        {type === "oss" ? (
-          <div className="flex flex-col gap-2">
-            <span className="font-semibold">Addressed Issue</span>
-            <ul className="w-full max-h-24 overflow-y-auto">
-              {project.repository?.issues?.map((issue) => (
-                <li key={issue.number} className="w-full">
-                  <a
-                    href={`${project.repository?.url}issues/${issue.number}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-block w-94 max-w-94 bg-zinc-50 border border-zinc-300 rounded-md mx-2 px-2 py-1 truncate"
-                  >
-                    {issue.title}
-                  </a>
-                </li>
+          <div className="flex flex-col items-start gap-2">
+            <span className="font-semibold">Tech Stack</span>
+            <div className="mx-2 flex flex-row flex-wrap gap-1">
+              {project.techStack.map((tech) => (
+                <span
+                  key={tech}
+                  className="bg-zinc-50 border border-zinc-300 rounded-xl text-xs mx-1 px-4 py-1"
+                >
+                  {tech}
+                </span>
               ))}
-            </ul>
+            </div>
           </div>
-        ) : (
-          ""
-        )}
+          {type === "oss" ? (
+            <div className="flex flex-col gap-2 w-full">
+              <span className="font-semibold">Addressed Issue</span>
+              <ul className="w-full max-h-24 overflow-y-auto flex flex-col gap-1 pr-1">
+                {project.repository?.issues?.map((issue) => (
+                  <li key={issue.number} className="w-full">
+                    <a
+                      href={`${project.repository?.url}issues/${issue.number}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="block w-full bg-zinc-50 border border-zinc-300 rounded-md px-2 py-1 truncate hover:bg-zinc-100"
+                    >
+                      {issue.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
+        </div>
+
         {hasProjectUrl ? (
-          <div className="flex flex-col items-start">
+          <div className="flex flex-col items-start pt-2">
             <span className="font-semibold">Link</span>
             <div className="mx-2 flex flex-row gap-2">
-              {project.link
-                ? (
-                  <a href={project.link} target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGlobe} />
-                  </a>
-                )
-                : null
-              }
-              {project.repository?.url
-                ? (
-                  <a href={project.repository.url} target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} />
-                  </a>
-                )
-                : null
-              }
+              {project.link ? (
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faGlobe} />
+                </a>
+              ) : null}
+              {project.repository?.url ? (
+                <a href={project.repository.url} target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              ) : null}
             </div>
           </div>
         ) : null}
