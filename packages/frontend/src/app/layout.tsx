d99111be1fe3cde0f7d400b5dd0d019e0697f1c1
namahu <neti0326@gmail.com>
2026-08-29 10:53:36 +0900

    feat: add footer

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/footer";
 import type React from "react";
 
 export type RootLayoutProps = {
@@ -13,6 +14,7 @@ export function RootLayout({ children }: RootLayoutProps) {
         </div>
       </header>
       <main className="w-full overflow-x-hidden">{children}</main>
+      <Footer />
     </div>
   );
 }
