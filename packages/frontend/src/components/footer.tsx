f8b7ff76e89f381f7fe4ea9288e4ad238343a922
namahu <neti0326@gmail.com>
2026-08-29 11:01:41 +0900

    fix: typo

--- packages/frontend/src/components/footer.tsx
+++ packages/frontend/src/components/footer.tsx
@@ -2,11 +2,11 @@ export function Footer() {
   const startYear = 2026;
   const now = new Date();
   const currentYear = now.getFullYear();
-  const yearRenge = startYear === currentYear ? startYear : `${startYear} ~ ${currentYear}`;
+  const yearRange = startYear === currentYear ? startYear : `${startYear} ~ ${currentYear}`;
 
   return (
-    <div className="text-center text-zinc-500 py-2">
-      <span>@ {yearRenge} namahu. All rights reserved.</span>
-    </div>
+    <footer className="text-center text-zinc-500 py-2">
+      <span>© {yearRange} namahu. All rights reserved.</span>
+    </footer>
   );
 };
